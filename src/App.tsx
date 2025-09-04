47b7c80cc13e8ffb02aa9c52f8d77708d80c50e1
hbhdhedhdthh <fortnitale@gmail.com>
2025-09-04 17:43:30 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { router } from "./routes";
+import { Analytics } from "@vercel/analytics/react"; // 👈 importa analytics
 
 const App = () => {
   return (
     <div className="min-h-screen">
       <RouterProvider router={router} />
+
+      {/* 👇 aquí va el componente de analíticas */}
+      <Analytics />
     </div>
   );
 };
